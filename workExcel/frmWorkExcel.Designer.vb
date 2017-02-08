fcdb8413d63561d9206e5904dadd60391ea050bd
Moises <moises_saus@hotmail.com>
2017-02-08 22:30:00 +0100

    Inici projecte

---
+++ workExcel/frmWorkExcel.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmWorkExcel
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.tbRuta = New System.Windows.Forms.TextBox()
+        Me.btnExaminar = New System.Windows.Forms.Button()
+        Me.btnExplorarExcel = New System.Windows.Forms.Button()
+        Me.lblContingutCela = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'tbRuta
+        '
+        Me.tbRuta.Location = New System.Drawing.Point(65, 45)
+        Me.tbRuta.Name = "tbRuta"
+        Me.tbRuta.Size = New System.Drawing.Size(260, 20)
+        Me.tbRuta.TabIndex = 0
+        '
+        'btnExaminar
+        '
+        Me.btnExaminar.Location = New System.Drawing.Point(350, 45)
+        Me.btnExaminar.Name = "btnExaminar"
+        Me.btnExaminar.Size = New System.Drawing.Size(75, 23)
+        Me.btnExaminar.TabIndex = 1
+        Me.btnExaminar.Text = "Examinar"
+        Me.btnExaminar.UseVisualStyleBackColor = True
+        '
+        'btnExplorarExcel
+        '
+        Me.btnExplorarExcel.Location = New System.Drawing.Point(184, 114)
+        Me.btnExplorarExcel.Name = "btnExplorarExcel"
+        Me.btnExplorarExcel.Size = New System.Drawing.Size(170, 35)
+        Me.btnExplorarExcel.TabIndex = 2
+        Me.btnExplorarExcel.Text = "Explorar Excel"
+        Me.btnExplorarExcel.UseVisualStyleBackColor = True
+        '
+        'lblContingutCela
+        '
+        Me.lblContingutCela.AutoSize = True
+        Me.lblContingutCela.Location = New System.Drawing.Point(184, 186)
+        Me.lblContingutCela.Name = "lblContingutCela"
+        Me.lblContingutCela.Size = New System.Drawing.Size(39, 13)
+        Me.lblContingutCela.TabIndex = 3
+        Me.lblContingutCela.Text = "Label1"
+        '
+        'frmWorkExcel
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(555, 261)
+        Me.Controls.Add(Me.lblContingutCela)
+        Me.Controls.Add(Me.btnExplorarExcel)
+        Me.Controls.Add(Me.btnExaminar)
+        Me.Controls.Add(Me.tbRuta)
+        Me.Name = "frmWorkExcel"
+        Me.Text = "Explora Excel"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents tbRuta As TextBox
+    Friend WithEvents btnExaminar As Button
+    Friend WithEvents btnExplorarExcel As Button
+    Friend WithEvents lblContingutCela As Label
+End Class
---
+++ workExcel/frmWorkExcel.vb
@@ -0,0 +1,54 @@
+﻿Imports Microsoft.Office.Interop
+Imports Microsoft.Office.Interop.Excel
+
+Public Class frmWorkExcel
+
+    'Private xlApp As Excel.Application
+    'Private xlLlibre As Excel.Workbook
+    'Private xlFulla As Excel.Worksheet
+
+
+    Private Sub frmWorkExcel_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        btnExplorarExcel.Enabled = False
+    End Sub
+
+    Private Sub btnExaminar_Click(sender As Object, e As EventArgs) Handles btnExaminar.Click
+        Dim openFileDialog1 As New OpenFileDialog()
+        openFileDialog1.Title = "Selecciona un fitxer"
+        If openFileDialog1.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+            tbRuta.Text = openFileDialog1.FileName.ToString()
+            btnExplorarExcel.Enabled = True
+        End If
+    End Sub
+
+    Private Sub btnExplorarExcel_Click(sender As Object, e As EventArgs) Handles btnExplorarExcel.Click
+        Dim xlApp = New Excel.Application
+        Dim xlLlibre As Excel.Workbook
+        Dim xlFulla As Excel.Worksheet
+
+        Dim numColumnes As Integer
+        Dim numFiles As Integer
+        xlLlibre = xlApp.Workbooks.Open(tbRuta.Text, True, True, , "")
+        xlFulla = xlApp.Worksheets("Hoja1")
+        numColumnes = xlFulla.Columns.Count
+        numFiles = xlFulla.Rows.Count
+
+        Dim contingutCela As String
+
+        For i As Integer = 1 To numColumnes
+            For j As Integer = 1 To numFiles
+                contingutCela = xlFulla.Range(i, j).Text.ToString()
+                lblContingutCela.Text = contingutCela
+            Next
+        Next
+
+        xlApp.Quit()
+        xlLlibre.Close()
+
+        xlApp = Nothing
+        xlLlibre = Nothing
+        xlFulla = Nothing
+
+
+    End Sub
+End Class
